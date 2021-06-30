51c3124182e5622e43ebcab9ebfadb85ca0d4d50
AnnieMcC <anne.mccloghry@gmail.com>
2021-06-30 11:06:19 +1000

    First commit

---
+++ MantelLogFileParser/MantelLogFileParser/Models/LogFileDataSummary.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MantelLogFileParser.Models
+{
+    public class LogFileDataSummary
+    {
+        public int UniqueIps { get; set; }
+
+        public List<string> Top3Urls { get; set; }
+
+        public List<Version> Top3Ips { get; set; }
+    }
+}
---
+++ MantelLogFileParser/MantelLogFileParser/Services/LogFileParserService.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using MantelLogFileParser.Models;
+
+namespace MantelLogFileParser.Services
+{
+    public interface ILogFileParserService
+    {
+        LogFileData GetLogData();
+    }
+
+    public class LogFileParserService : ILogFileParserService
+    {
+        private readonly IDataFileService _dataFileService;
+
+        public LogFileParserService(IDataFileService dataFileService)
+        {
+            _dataFileService = dataFileService;
+        }
+
+        public LogFileData GetLogData()
+        {
+            var logFileData = new LogFileData();
+
+            try
+            {
+                var fileResult = _dataFileService.GetDataFromFile();
+
+                if (fileResult != null && fileResult.Any())
+                {
+                    logFileData = ParseLogFileData(fileResult);
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            Console.WriteLine(logFileData);
+
+            return logFileData;
+        }
+
+        public LogFileData ParseLogFileData(string[] fileResult)
+        {
+            var httpRequests = new List<HttpRequestModel>();
+
+            foreach (var logItem in fileResult)
+            {
+                if (!string.IsNullOrEmpty(logItem))
+                {
+                    var httpRequestModel = new HttpRequestModel();
+                    httpRequests.Add(httpRequestModel.CreateFrom(logItem));
+                }
+            }
+
+            return new LogFileData { HttpRequests = httpRequests };
+        }
+    }
+}
