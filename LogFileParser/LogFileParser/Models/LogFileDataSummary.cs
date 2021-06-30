740be0fb5519d91c9d783ca9f1b58a3193b67564
AnnieMcC <anne.mccloghry@gmail.com>
2021-06-30 10:27:38 +1000

    Log File Parser code test
    First commit

---
+++ LogFileParser/LogFileParser/Models/LogFileDataSummary.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LogFileParser.Models
+{
+    public class LogFileDataSummary
+    {
+        public int UniqueIps { get; set; }
+
+        public List<string> Top3Urls { get; set; }
+
+        public List<Version> Top3Ips { get; set; }
+    }
+}
